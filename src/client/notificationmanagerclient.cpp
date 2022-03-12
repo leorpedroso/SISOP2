98d0645748b6d640736e76196f32743f85c17179
guidytz <guidytz@gmail.com>
2022-03-12 11:45:01 -0300

    Correção nos splits

--- src/client/notificationmanagerclient.cpp
+++ src/client/notificationmanagerclient.cpp
@@ -19,19 +19,22 @@ void NotificationManager::listen(){
             spMessage = sock.splitUpToMessage(spMessage[1], 4);
             std::string notification = "@" + spMessage[1] + " " + spMessage[2] + "\n " + spMessage[3];
             interface.updateNotifications(stoi(spMessage[0]), notification);
-        } else if (type == sock.ACK) { // verify if message is just an ACK from server
-            spMessage = sock.splitUpToMessage(spMessage[1], 2);
+        } else if (type == sock.ACK) { 
+            // 3. if message is an ACK from operations SEND or FOLLOW
+            spMessage = sock.splitUpToMessage(spMessage[1], 3);
+            
             if (spMessage[0] == "SEND") {
-                spMessage = sock.splitUpToMessage(spMessage[1], 2);
+                // 3.1. if ACK is from SEND, print timestamp and message that the server received
                 std::cout << "Server received message \"";
-                std::cout << spMessage[1] << "\" at " << spMessage[0] << std::endl;
+                std::cout << spMessage[2] << "\" at " << spMessage[1] << std::endl;
             } else if (spMessage[0] == "FOLLOW") {
+                // 3.2 if ACk is from FOLLOW, print feedback from operation and user involved
                 if (spMessage[1] == "1") 
-                    std::cout << "Profile \'" << spMessage[3] << "\' followed." << std::endl;
+                    std::cout << "Profile \'" << spMessage[2] << "\' followed." << std::endl;
                 else if (spMessage[1] == "0")
-                    std::cout << "Profile \'" << spMessage[3] << "\' not found in server." << std::endl;
+                    std::cout << "Profile \'" << spMessage[2] << "\' not found in server." << std::endl;
                 else if (spMessage[1] == "2") 
-                    std::cout << "You already follow profile \'" << spMessage[3] << "\'." << std::endl;
+                    std::cout << "You already follow profile \'" << spMessage[2] << "\'." << std::endl;
                 else 
                     std::cout << "ERROR " << input << std::endl; 
             }
